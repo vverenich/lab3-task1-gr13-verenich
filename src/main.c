c0631f8b6c7d7782e27a8072d7a2f0d932e1b28b
vverenich <>
2022-03-13 07:11:58 +0000

    add main finction

--- src/main.c
+++ src/main.c
@@ -1,8 +1,25 @@
-/* hello.c */
+/* main.c */
 #include <stdio.h>
+#include <string.h>
 
-void main (void)
+int main (int argc, char *argv[])
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+  
+  if (argc < 3){
+    return 0;
+  }
+
+  char requiredSymbol = argv[1][0];
+  int numberOfWords   = argc - 2;
+
+  printf("%c", requiredSymbol);
+  
+  int count = 0;
+  for(int i = 0; i < numberOfWords; ++i){
+    count += getCountOfParticularCharInWord(requiredSymbol, argv[i+2]); 
+  }
+  
+	printf ("The count of symbol %c is %d", requiredSymbol, count);
+  
+  return 0;
 }
